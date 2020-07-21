e933850bc17fc53dd5988623d51f6a5c62e1f449
Michael Callahan <CallahanMichaelE@gmail.com>
2020-07-21 08:35:17 -0600

    test running for binary but failing

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -7,15 +7,24 @@ def linear_search(arr, target):
             i += 1
     return -1
     
-        
-
-
-
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    firstIndex = 0
+    lastIndex = len(arr) - 1
 
-    # Your code here
+    if len(arr) == 0:
+        return -1
+
+    while firstIndex < lastIndex:
+        midpoint = int((lastIndex - firstIndex) / 2) 
+
+        if target is arr[midpoint]:
+            return midpoint
+        elif target > arr[midpoint]:
+            firstIndex = midpoint + 1
+        else:
+            lastIndex = midpoint - 1
 
 
     return -1  # not found
